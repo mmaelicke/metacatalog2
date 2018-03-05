1eb4a7743d95671a5102b486d3cfbe4c47876333
Mirko Mälicke <mirko.maelicke@kit.edu>
2018-03-05 06:56:29 +0100

    expanded elasticsearch_dsl DocType model

--- metacatalog2/elastic.py
+++ metacatalog2/elastic.py
@@ -2,9 +2,50 @@ import os
 
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
+from elasticsearch_dsl import DocType as ElasticDocType
 
 new_elastic = lambda: Elasticsearch(os.environ.get('ELASTIC_NODE', 'http://localhost:9200'))
 es = new_elastic()
 
 # build a Search object
 search = Search(using=es)
+
+
+# build a custom DocType class, which is extended by some useful fuctions
+class DocType(ElasticDocType):
+    @classmethod
+    def all(cls, as_hit=False):
+        """
+        Return all instances of this DocType objects from the index.
+        Note: it does by default set the hit return range to 0:total_hits, that might cause some
+        traffic for large indices.
+        The query is implemented by the `elasticsearch_dsl.Search.execute` method with caching activated.
+        Nevertheless, the search object is re-created on every call, therefore I am not sure if the
+        hits are actually cached.
+        By default, the hits will be converted into the calling clss `cls`, if as_hit is True, they will
+        be returned as search hit objects (including the score of 1).
+
+        :return: list, all documents in the index
+        :param as_hit: bool, if True the instances will be returned as `elasticsearch_dsl.response.hit.Hit`, else as
+                `elasticsearch_dsl.DocType`.
+        """
+        s = cls.search()
+
+        # overwrite the default result range of 10 hits
+        s = s[0:s.count()]
+
+        # execute a empty search on the context document type
+        if as_hit:
+            return [hit for hit in s.execute()]
+        else:
+            return cls.mget([hit.meta.id for hit in s.execute()])
+
+    def to_json(self):
+        """
+        This method does just wrap the to_dict method with :param include_meta: set to True by default.
+        Can be used to overwrite the json output by inheriting classes.
+
+        :return: dict of the document
+        """
+        return self.to_dict(include_meta=True)
+
--- metacatalog2/models.py
+++ metacatalog2/models.py
@@ -1,6 +1,6 @@
-from elasticsearch_dsl import DocType, Text
+from elasticsearch_dsl import Text
 
-from metacatalog2.elastic import es
+from metacatalog2.elastic import es, DocType
 
 
 class Context(DocType):
@@ -12,42 +12,6 @@ class Context(DocType):
         index = 'mgt'
         doc_type = 'context'
         using = es
-
-    @staticmethod
-    def all():
-        """
-        Return all instances of Context objects from the index.
-        If you need an iterator over all instances, use the `Context.iterall` method.
-
-        :return: list, all Context documents in the index
-        """
-        s = Context.search()
-
-        # overwrite the default result range of 10 hits
-        s = s[0:s.count()]
-
-        # execute a empty search on the context document type
-        return [hit for hit in s.execute()]
-
-    @staticmethod
-    def iterall():
-        """
-        Return an iterator over all instances.
-        If you need all hits as a list, use the `Context.all` method.
-
-        Note: at the current stage, this method will retrieve all documents and iterate over them.
-        Thus, there won't be a performance gain. The method is defined for a future implementation of
-        a iterator using the elasticsearch pagination.
-
-        :return: iterator over all Context documents in the index
-        """
-        s = Context.search()
-
-        #overwrite the default result range of 10 hits.
-        s = s[0:s.count()]
-
-        for hit in s.execute():
-            yield hit
 
     # customize the save method
     def save(self, **kwargs):
